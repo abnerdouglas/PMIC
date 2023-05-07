c044097a7531b7ec9e24b02d72194377629af414
Abner <abner.rmachado@gmail.com>
2023-05-07 18:31:32 -0300

    desafio3

--- meuprojeto/app.py
+++ meuprojeto/app.py
@@ -1,18 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
+
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__)
+Bootstrap(app)
+
+
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
 
 @app.route("/")
+@app.route("/index")
 def home():
-    return render_template('home.html')
-
-@app.route("/contato")
-def contato():
-    return render_template('contato.html')
+    return render_template("home.html")
 
 @app.route("/quemsomos")
-def quemsomos():
-    return render_template('quemsomos.html')
+def quem_somos():
+    return render_template("quemsomos.html")
 
-if __name__ == "__main__":
-    app.run(port=5000,debug=True,use_debugger=False, use_reloader=False)
+'''
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+'''
+
+
+@app.route('/contatos', methods=['GET', 'POST'])
+def contatos():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contatos(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
+        mysql.connection.commit()
+        
+        cur.close()
+
+        return 'sucesso'
+    return render_template('contatos.html')
+
+
+# rota usuários para listar todos os usuário no banco de dados.
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
